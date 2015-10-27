0ce5fda0eae0de06ab88244b50581941189c9c51
nazar-ivantsiv <nazar.ivantsiv@gmail.com>
2015-10-27 23:06:02 +0200

    init commit. function works. implemented without a GENERATOR

--- count_unique_n_replace_words.py
+++ count_unique_n_replace_words.py
@@ -8,13 +8,29 @@ import os
 
 WORK_DIR = os.getcwd()
 
-def read_file_into_string(file_name):
-  string_of_file = ''
-  path_to_file = os.path.join(WORK_DIR, file_name)
-  file_to_read = open(path_to_file, 'r')
-  for line in file_to_read:
-    string_of_file += line
-  file_to_read.close()
-  return string_of_file
+def global_replacement(file_name, origina_word, replace_word):
+  '''
+  Replaces the <origina_word> with <replace_word> in file <file_name>.
 
-read_file_into_string('lorem.txt')
+  file_name - name of the file in working dir
+  origina_word - a word to seek for
+  replace_word - a word to replace
+  '''
+
+  path_to_file = os.path.join(WORK_DIR, file_name)
+  file_read = open(path_to_file, 'r')
+  data = ''
+
+  for line in file_read:
+    line = line.replace(origina_word, replace_word)
+    data += line
+
+  file_read.close()
+
+  file_write = open(path_to_file, 'w')
+  file_write.write(data)
+  file_write.close()
+
+
+global_replacement('lorem.txt', 'ut', 'XXX')
+
