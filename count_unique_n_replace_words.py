f3fdd71f5d7834c577977bbce3b0b65a8d06c9d9
nazar-ivantsiv <nazar.ivantsiv@gmail.com>
2015-10-28 00:32:40 +0200

    Implementation through instance of iterable. Creates extra output file - BAD

--- count_unique_n_replace_words.py
+++ count_unique_n_replace_words.py
@@ -5,12 +5,14 @@ Authors:
   Andriy Bondarev
 """
 import os
+from itertools import imap
 
 WORK_DIR = os.getcwd()
 
 def global_replacement(file_name, origina_word, replace_word):
   '''
-  Replaces the <origina_word> with <replace_word> in file <file_name>.
+  Replaces the <origina_word> with <replace_word> in original file <file_name> & saves it to 
+  <file_name>_mod.txt .
 
   file_name - name of the file in working dir
   origina_word - a word to seek for
@@ -18,19 +20,20 @@ def global_replacement(file_name, origina_word, replace_word):
   '''
 
   path_to_file = os.path.join(WORK_DIR, file_name)
-  file_read = open(path_to_file, 'r')
-  data = ''
+  path_to_mod_file = os.path.join(WORK_DIR, file_name.split('.')[0]+'_mod.txt')	# Adds '_mod' to the filename
 
-  for line in file_read:
-    line = line.replace(origina_word, replace_word)
-    data += line
+  file_to_read = open(path_to_file, 'r')
 
-  file_read.close()
-
-  file_write = open(path_to_file, 'w')
-  file_write.write(data)
-  file_write.close()
-
+  replace_the_word = lambda x: x.replace(origina_word, replace_word)
+  changed_file = imap(replace_the_word, file_to_read)							# Creates iterable with changes
+  
+  file_to_write = open(path_to_mod_file, 'w')
+  
+  for line in changed_file:
+    file_to_write.write(line)
+  
+  file_to_read.close()
+  file_to_write.close()
 
 global_replacement('lorem.txt', 'ut', 'XXX')
 
