76911eebb66807a466449a7b6f34daebf91d1f0a
nazar-ivantsiv <nazar.ivantsiv@gmail.com>
2015-10-29 21:47:13 +0200

    FIle routines included inside the function. Default value for out_file defined.

--- count_unique_n_replace_words.py
+++ count_unique_n_replace_words.py
@@ -10,24 +10,35 @@ from itertools import imap
 WORK_DIR = os.getcwd()
 
 
-def global_replacement(input_file, output_file, original_word, replace_word):
+def global_replacement(original_word, replace_word, input_filename, output_filename=''):
   '''
-  Replaces the <origina_word> with <replace_word>...
+  Replaces the <origina_word> with <replace_word> in <input_filename> and saves
+  the changes to <output_filename> (or to <input_filename>_mod.txt).
 
-  input_file - handle of the INPUT file (fileObject)
-  output_file - handle of the OUTPUT file (fileObject)
+  input_filename - handle of the INPUT file (fileObject)
+  output_filename - handle of the OUTPUT file (fileObject)
   original_word - a word to seek for
   replace_word - a word to replace
   '''
-
-  replace_the_word = lambda x: x.replace(original_word, replace_word)
-  changed_file = imap(replace_the_word, input_file)							# Creates iterable with changes  
+  input_path = os.path.join(WORK_DIR, input_filename)
+  if output_filename == '':	
+  	output_path = os.path.join(WORK_DIR, input_filename.split('.')[0]+'_mod.txt')	# Adds '_mod' to the input_filename
+  else:
+    output_path = os.path.join(WORK_DIR, output_filename)
   
-  while True:
-    try:
-      output_file.write(changed_file.next())
-    except StopIteration:
-  	  break
+  file_to_read = open(input_path, 'r')
+  file_to_write = open(output_path, 'w')
+
+  changed_file = imap( lambda x: x.replace(original_word, replace_word), file_to_read )			# Creates generator to output strs with words changed
+
+  while True:		# Using this iteration through the Generator NOT too use any temp variables
+      try:
+        file_to_write.write(changed_file.next())
+      except StopIteration:
+        break
+
+  file_to_read.close()
+  file_to_write.close()
 
 
 def make_uniques_dict(input_file):
@@ -47,29 +58,12 @@ def make_uniques_dict(input_file):
   return uniques
 
 
-#def update_dict(dict1, dict2):
-#  for key in dict2.keys():
-#    if key in dict1:
-#	    dict1[key] += dict2[key]
-#	  else:
-#       dict1[key] = dict2[key]
-#	return dict1
-
-file_name = 'lorem.txt'
-path_to_file = os.path.join(WORK_DIR, file_name)
-path_to_mod_file = os.path.join(WORK_DIR, file_name.split('.')[0]+'_mod.txt')	# Adds '_mod' to the filename
-
 # GLOBAL REPLACE TEST
-file_to_read = open(path_to_file, 'r')
-file_to_write = open(path_to_mod_file, 'w')
 
-global_replacement(file_to_read, file_to_write, 'at', 'FFF')
-
-file_to_read.close()
-file_to_write.close()
+global_replacement('at', 'FFF', 'lorem.txt')
 
 # MAKE UNIQUES TEST
-file_to_read = open(path_to_file, 'r')
-print(make_uniques_dict(file_to_read))
+#file_to_read = open(path_to_file, 'r')
+#print(make_uniques_dict(file_to_read))
 
-file_to_read.close()
+#file_to_read.close()
